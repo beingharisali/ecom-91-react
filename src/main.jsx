d374b3aba78c2eac891a57d29651674a4bbc13ad
beingharisali <theharis99@gmail.com>
2026-07-05 14:29:31 +0500

    updated vite config file

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import.meta.env.MODE;
+import.meta.env.DEV;
+import.meta.env.PROD;
+import.meta.env.VITE_SOME_VALUE;
 
 createRoot(document.getElementById("root")).render(<App />);
